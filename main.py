40dcd0ca042a54c45edc945d273a82de52655d85
andrei1112111 <74547328+andrei1112111@users.noreply.github.com>
2021-12-04 12:43:19 +0700

    12:43 04.11.21

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from map import map1
 
 pygame.init()
 pygame.display.set_caption('game base')
@@ -6,14 +7,8 @@ screen = pygame.display.set_mode((900, 900), 0, 32)
 display = pygame.Surface((300, 300))
 clock = pygame.time.Clock()
 
-# map1 = [[1 for _ in range(15)] for _ in range(5)]
-# map2 = [[0 for _ in range(15)] for _ in range(5)]
-map1 = [[[1, 0, 1], [0, 1, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]]
-# map2 = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-
 from texture import get_texture
 from player import Player
-
 
 player = Player()
 
@@ -28,7 +23,7 @@ while True:
             for z, tile in enumerate(height):
                 if tile:
                     display.blit(get_texture(tile), ((150 + x * 10 - y * 10) + x_pos,  # X
-                                                     (100 + x * 5 + y * 5 - ((z+1) * 14)) + y_pos))  # y
+                                                     (100 + x * 5 + y * 5 - ((z + 1) * 14)) + y_pos))  # y
 
     player.get_move()
 
--- player.py
+++ player.py
@@ -1,20 +1,39 @@
 import pygame
+from map import map1
 
 
 class Player:
     def __init__(self):
         self.x_pos = 0
         self.y_pos = 0
+        self.img = pygame.image.load('hero.png').convert()
+        self.img.set_colorkey((0, 0, 0))
 
     def get_move(self):
-        for i in pygame.event.get():
-            if i.type == pygame.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(self.get_cords(event.pos))
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             self.y_pos += 1
         elif keys[pygame.K_s]:
             self.y_pos -= 1
+        if keys[pygame.K_a]:
+            self.x_pos += 1
+        elif keys[pygame.K_d]:
+            self.x_pos -= 1
 
     def get_pos(self):
         return self.x_pos, self.y_pos
+
+    def get_cords(self, pos):
+        for y, row in enumerate(map1):
+            for x, height in enumerate(row):
+                for z, tile in enumerate(height):
+                    if tile == 1:
+                        xx = (((150 + x * 10 - y * 10) + self.x_pos) * 3)
+                        yy = (((100 + x * 5 + y * 5 - ((z + 1) * 14)) + self.y_pos) * 3)
+                        if xx < pos[0] < xx+20*3 and yy < pos[1] < yy+10*3:
+                            return x, y
